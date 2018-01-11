e64054ffd1c312e1b8fb9d77cd511c864b60838e
Matthew Daly <matthewbdaly@gmail.com>
2018-01-11 16:47:36 +0000

    Added form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,6 +14,17 @@
                         </div>
                     @endif
 
+                    <form class="form-horizontal" method="POST" action="/create/">
+                        {{ csrf_field() }}
+                        <div class="form-group{{ $errors->has('url') ? ' has-error' : '' }}">
+                            <label for="url" class="control-label">URL</label>
+                            <input type="url" name="url" placeholder="Page to save" required>
+                            <button type="submit" class="btn btn-primary">
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+
                     <ul class="list-group">
                         @foreach ($links as $link)
                         <li class="list-group-item">
