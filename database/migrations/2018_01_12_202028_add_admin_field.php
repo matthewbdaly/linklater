366f8dc81b3a312b8abdb86afaec5f8fa5bd610f
Matthew Daly <matthewbdaly@gmail.com>
2018-01-12 20:25:50 +0000

    Fixed broken migration

--- database/migrations/2018_01_12_202028_add_admin_field.php
+++ database/migrations/2018_01_12_202028_add_admin_field.php
@@ -13,7 +13,7 @@ class AddAdminField extends Migration
      */
     public function up()
     {
-        Schema::table('users' function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->boolean('admin')->default(false);
         });
     }
@@ -25,7 +25,7 @@ class AddAdminField extends Migration
      */
     public function down()
     {
-        Schema::table('users' function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('admin');
         });
     }
