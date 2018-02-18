15401b5470a01b4a4ee51a2d8a6dcbb89dd00403
Matthew Daly <matthewbdaly@gmail.com>
2018-02-18 19:32:32 +0000

    Fixed an issue with fetching pages over HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace LinkLater\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use LinkLater\Services\Fetcher;
+use GuzzleHttp\Client as GuzzleClient;
+use Http\Adapter\Guzzle6\Client;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,16 +26,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(
-            'LinkLater\Contracts\Services\Fetcher',
-            'LinkLater\Services\Fetcher'
-        );
-       $this->app->singleton('LinkLater\Contracts\Repositories\Link', function () {
+        $this->app->singleton('LinkLater\Contracts\Services\Fetcher', function () {
+            $guzzle = new GuzzleClient([
+                'verify' => false
+            ]);
+            $client = new Client($guzzle);
+            return new Fetcher($client);
+        });
+        $this->app->singleton('LinkLater\Contracts\Repositories\Link', function () {
             $baseRepo = new \LinkLater\Eloquent\Repositories\Link(new \LinkLater\Eloquent\Models\Link);
             $cachingRepo = new \LinkLater\Eloquent\Repositories\Decorators\Link($baseRepo, $this->app['cache.store']);
             return $cachingRepo;
         });
-       $this->app->singleton('LinkLater\Contracts\Repositories\User', function () {
+        $this->app->singleton('LinkLater\Contracts\Repositories\User', function () {
             $baseRepo = new \LinkLater\Eloquent\Repositories\User(new \LinkLater\Eloquent\Models\User);
             $cachingRepo = new \LinkLater\Eloquent\Repositories\Decorators\User($baseRepo, $this->app['cache.store']);
             return $cachingRepo;
