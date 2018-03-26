72b520909bc8b61cd82463182cb5e78f4068ae75
Matthew Daly <matthewbdaly@gmail.com>
2018-03-26 22:12:02 +0100

    Amended styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,9 @@
 
 @section('content')
 <div class="container">
+    <div class="jumbotron">
+        <h1><a href="/">LinkLater</a></h1>
+        <h2>Save your links for later</h2>
+    </div>
 </div>
 @endsection
