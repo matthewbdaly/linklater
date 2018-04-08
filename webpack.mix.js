a6d868eec893c4bf4ec49bf2b36a69c4338875b7
Matthew Daly <matthewbdaly@gmail.com>
2018-04-08 21:05:03 +0100

    Generated React preset

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,5 @@ let mix = require('laravel-mix');
  |
  */
 
-mix
-   .react('resources/assets/js/frontend.js', 'public/js')
-   .js('resources/assets/js/app.js', 'public/js')
+mix.react('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
