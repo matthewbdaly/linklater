c45b62c91be88ed14b4561510c0f29da42867fd0
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-05-26 15:50:41 +0100

    Finished getting ESLint in place

--- resources/assets/js/reducer.js
+++ resources/assets/js/reducer.js
@@ -2,12 +2,13 @@ import {List, Map, fromJS} from 'immutable';
 
 export default function(state = Map(), action) {
   switch (action.type) {
-    case 'ADD_LINK':
-      let links = state.get('links').push(fromJS(action.content));
-      return state.set('links', links);
-    case 'UPDATE_FILTER':
-      return state.set('filter', fromJS(action.content));
-    default:
-      return state;
+  case 'ADD_LINK': {
+    let links = state.get('links').push(fromJS(action.content));
+    return state.set('links', links);
+  }
+  case 'UPDATE_FILTER':
+    return state.set('filter', fromJS(action.content));
+  default:
+    return state;
   }
 }
