4ec597f80551b50fdbfc6efb0ba297ad7663ef14
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-05-26 20:43:07 +0100

    Added another spec

--- __tests__/reducer.spec.js
+++ __tests__/reducer.spec.js
@@ -2,6 +2,17 @@ import reducer from '../resources/assets/js/reducer';
 import {Map, fromJS} from 'immutable';
 
 describe('Reducer', () => {
+  it('handles default path', () => {
+    const initialState = Map();
+    const action = {
+      type: 'FOO',
+    };
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).toEqual(fromJS({
+    }));
+  });
+
   it('handles UPDATE_FILTER', () => {
     const initialState = Map();
     const action = {
