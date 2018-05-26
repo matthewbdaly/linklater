2f0eed83a221c8b5e065b6a7206b5b15b4f07c10
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-05-26 20:23:32 +0100

    Added another snapshot test

---
+++ __tests__/components/LinkInput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {findDOMNode} from 'react-dom';
+import LinkInput from '../../resources/assets/js/components/LinkInput';
+import renderer from 'react-test-renderer';
+
+describe('LinkInput', () => {
+  it('renders the link input', () => {
+    const store = jest.fn();
+    const tree = renderer
+      .create(<LinkInput storeLink={store} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
