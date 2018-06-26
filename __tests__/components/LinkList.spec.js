39c91acabc96390d12e8463e03727c15f824628b
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2018-06-26 21:03:23 +0100

    Improved coverage for one file

--- __tests__/components/LinkList.spec.js
+++ __tests__/components/LinkList.spec.js
@@ -3,10 +3,28 @@ import {findDOMNode} from 'react-dom';
 import LinkList from '../../resources/assets/js/components/LinkList';
 import renderer from 'react-test-renderer';
 
+const links = [{
+  title: 'My link',
+  link: 'http://example.com',
+  id: 1
+}, {
+  title: 'Something else',
+  link: 'https://example.com',
+  id: 2
+}];
 describe('LinkList', () => {
   it('renders the link list', () => {
+    const filter = '';
     const tree = renderer
-      .create(<LinkList links={[]} />)
+      .create(<LinkList filter={filter} links={links} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders a non-empty list', () => {
+    const filter = 'My link';
+    const tree = renderer
+      .create(<LinkList filter={filter} links={links} />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
